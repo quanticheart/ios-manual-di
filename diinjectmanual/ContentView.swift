27493c30b5a1a1cfbeb813a9b43417e2e0776191
Jonn Alves <jonatas.alsantos@jsl.com.br>
2023-02-20 16:02:11 -0300

    Initial Commit

---
+++ diinjectmanual/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  diinjectmanual
+//
+//  Created by Jonn Alves on 20/02/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ diinjectmanual/diinjectmanualApp.swift
@@ -0,0 +1,17 @@
+//
+//  diinjectmanualApp.swift
+//  diinjectmanual
+//
+//  Created by Jonn Alves on 20/02/23.
+//
+
+import SwiftUI
+
+@main
+struct diinjectmanualApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
